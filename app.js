402bf74dfae0aabf49b3613b53c6966fede5c812
Sharime Cahuas <sharimecahuas@gmail.com>
2023-06-11 14:58:34 -0300

    adding burger menu funcionalities

--- app.js
+++ app.js
@@ -5,15 +5,39 @@ const $$ = (selector) => document.querySelectorAll(selector)
 const cleanContainer = (selector) => $(selector).innerHTML = ''
 const hideElements = (selectors) => {
     for (const eachSelector of selectors) {
-        $(selector).classList.add('hidden')
+        $(eachSelector).classList.add('hidden')
     }
 }
 
 const showElements = (selectors) => {
     for (const eachSelector of selectors) {
-        $(selector).classList.remove('hidden')
+        $(eachSelector).classList.remove('hidden')
     }
 }
 
+const getData = (key) => JSON.parse(localStorage.getItem(key))
+const setData = (key, array) => localStorage.setItem(key, JSON.stringify(array))
 
+// EVENTS
 
+const initializeApp = () => {
+    const clickOnBurger = () => {
+        showElements(["#nav-menu-container", "#close-burger-menu"])
+        hideElements(["#show-burger-menu"])
+    }
+    
+    const clickOnClose = () => {
+        hideElements(["#nav-menu-container", "#close-burger-menu"])
+        showElements(["#show-burger-menu"])
+    }
+
+    $("#btn-menu").addEventListener("click", () => {
+        if ($("#show-burger-menu").classList.contains("hidden")) {
+            clickOnClose()
+        } else {
+            clickOnBurger()
+        }
+    }
+)}
+
+window.addEventListener("load", initializeApp)
